b05757ef31bb206b891eb6a3308ec5ec879a4330
Fujita-droid <66514859+Fujita-droid@users.noreply.github.com>
2021-03-16 13:42:00 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,54 @@
+$(function(){
+    $('.js-modal-open').each(function(){
+        $(this).on('click',function(){
+            var target = $(this).data('target');
+            var modal = document.getElementById(target);
+            $(modal).fadeIn();
+            return false;
+        });
+
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    }); 
+});
+
+$(function() {
+    var Accordion = function(el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+  
+      // Variables privadas
+      var links = this.el.find('.link');
+      // Evento
+      links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
+  
+    Accordion.prototype.dropdown = function(e) {
+      var $el = e.data.el;
+        $this = $(this),
+        $next = $this.next();
+  
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+  
+      if (!e.data.multiple) {
+        $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+      };
+    } 
+  
+    var accordion = new Accordion($('#accordion'), false);
+  });
+
+ 
+
+
+
+// ハンバーガーメニュー
+  //チェックボックスのチェックを外す
+$(function() {
+    $('.drawer-item>a').click(function() {
+      $('#drawer-check').removeAttr('checked').prop('checked', false).change();
+    });
+  });
